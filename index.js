656771642472f55f3925fe994a15ddacbdbec064
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-12 06:26:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function takeANumber (katzDeliLine, name) {
   }
   return message;
 }
+var katzDeliLine = [];
+var index=0;
+var nameArr = [];
+function takeANumber (katzDeliLine, name) {
+  nameArr = name.split(",");
+  for (index=0; index < nameArr.length; index++) {
+  katzDeliLine[index] = nameArr[index];
+  var place = katzDeliLine.length + index;
+  var message = `Welcome, ${nameArr[index]}. You are number ${place} in line.`;
+  console.log(nameArr[index]);
+  }
+  return message;
+}
