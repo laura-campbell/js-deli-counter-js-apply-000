4f213a411c80440ed013af4df86c95f501b6c954
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-12 06:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+function nowServing (katzDeliLine) {
+  return katzDeliLine[0];
+  
+}
