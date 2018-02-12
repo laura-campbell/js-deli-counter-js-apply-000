6619459efa7cdd6d991794f59e5f4d56c7aa3920
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-12 07:02:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function nowServing (katzDeliLine) {
   else {
   for (index=0; index<katzDeliLine.length; index++) {
   var customer = katzDeliLine[0];
-  console.log(`Currently serving ${customer}.`);
   katzDeliLine.splice(0,1);
   }
+  return `Currently serving ${customer}.`;
   }
 }
