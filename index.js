c0543d4ed7356621ca9db62f374563946e97760e
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-12 06:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 var index=0;
 var nameArr = [];
 function takeANumber (katzDeliLine, name) {
-  nameArr = name.split;
+  nameArr = name.split(",");
   for (index=0; index < nameArr.length; index++) {
   katzDeliLine[index] = nameArr[index];
   var place = katzDeliLine.length;
