d937ac914369c3c2957a602559f15ea21b02363e
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-12 06:00:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber (katzDeliLine, name) {
   nameArr[index] = name;
   for (index=0; index < nameArr.length; index++) {
   katzDeliLine[index] = name;
+  var newDeliLine = katzDeliLine.length + 1;
   var place = katzDeliLine.length;
   var message = `Welcome, ${nameArr[index]}. You are number ${place} in line.`
   }
